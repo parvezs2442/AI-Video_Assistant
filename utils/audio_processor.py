b0784435ab953fa510e5f0438475849d5e13f76b
Parvez Saifi <parvezs2442@gmail.com>
2026-08-13 23:07:33 +0530

    feat: add audio video to wav conversion

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -2,13 +2,11 @@ import yt_dlp
 from pydub import AudioSegment
 import os
 
+#Extract/download the audio from any video using link/url
 
 DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
-
-
 FFMPEG_PATH = r"C:\Users\PARVEZ SAIFI\Downloads\ffmpeg-master-latest-win64-gpl\ffmpeg-master-latest-win64-gpl\bin"
-
 
 def download_youtube_audio(url: str) -> str:
 
@@ -16,15 +14,11 @@ def download_youtube_audio(url: str) -> str:
         DOWNLOAD_DIR,
         "%(title)s.%(ext)s"
     )
-
     ydl_opts = {
         "format": "bestaudio/best",
-
         "outtmpl": output_path,
-
         # FFmpeg location
         "ffmpeg_location": FFMPEG_PATH,
-
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
@@ -32,30 +26,34 @@ def download_youtube_audio(url: str) -> str:
                 "preferredquality": "192",
             }
         ],
-
-        "quiet": True,
+        "quiet": True, #removes unnecessary consoles
         "noplaylist": True,
     }
-
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
         info = ydl.extract_info(
             url,
             download=True
         )
-
         filename = ydl.prepare_filename(info)
-
         filename = filename.replace(".webm", ".wav")
         filename = filename.replace(".m4a", ".wav")
-
     return filename
-
 
 if __name__ == "__main__":
 
-    youtube_url = "https://www.youtube.com/watch?v=_HQ2H_0Ayy0"
-
+    youtube_url = "https://youtu.be/VoKFyB1q4fc?si=furlIGasC-Ypw3OC"
     audio_file = download_youtube_audio(youtube_url)
+   
 
-    print(f"✅ Audio downloaded successfully: {audio_file}")
+
+def convert_to_wav(input_path: str) -> str:
+    """Convert any audio/video file to WAV format using pydub."""
+    output_path = os.path.splitext(input_path)[0] + "_converted.wav"
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1).set_frame_rate(16000)
+    audio.export(output_path, format="wav")
+
+    return output_path
+
+print(convert_to_wav(audio_file))
